43feac4f77f262db88562fefe4c1f92e6d828a00
Komal <110466516+PrjKomal@users.noreply.github.com>
2024-11-15 09:31:59 +0530

    remove console.log

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -1,5 +1,4 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log("locations", locations);
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
 
